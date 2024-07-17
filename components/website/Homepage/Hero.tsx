ed82afb359ebc6f6d63be457e6702d8e00facc0b
KiranNepali <itsmekiran44@gmail.com>
2024-07-17 20:10:06 +0545

    try video in hero

--- components/website/Homepage/Hero.tsx
+++ components/website/Homepage/Hero.tsx
@@ -31,15 +31,24 @@ export default function Hero({}: Props) {
   });
   return (
     <div className="w-11/12 mx-auto h-[95vh] mt-[4.2rem] rounded-3xl overflow-hidden relative text-white flex justify-center items-center">
-      <div className="absolute bottom-0 left-0 w-full h-[30vh] bg-gradient-to-t from-black to-transparent z-10"></div>
+      <div className="absolute bottom-0 left-0 w-full h-[15vh] bg-gradient-to-t from-black to-transparent z-20"></div>
       {/* image  */}
-      <div>
+      {/* <div>
         <Image
           src={Banner}
           alt=""
           className="absolute top-0 left-0 w-full h-full object-cover object-center brightness-50"
         ></Image>
-      </div>
+      </div> */}
+      <video
+        className="video-zoom absolute  w-full scale-y-95 h-screen  brightness-[0.5] z-10 object-cover object-center"
+        autoPlay
+        loop
+        muted
+      >
+        <source src="/hero.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       {/* middle  */}
       <div className="flex flex-col w-full gap-10">
         <div className="text-center z-10  relative  text-white   mx-auto flex justify-center items-center flex-col gap-2">
@@ -54,21 +63,21 @@ export default function Hero({}: Props) {
           </p>
         </div>
         {/* search  */}
-        <div className="w-[30%] rounded-3xl flex justify-center   items-center  overflow-hidden shadow-[rgba(17,_17,_26,_0.6)_0px_0px_16px]   relative z-10 h-[5vh] mx-auto ">
+        <div className="w-[30%]  flex justify-center   items-center  overflow-hidden shadow-[rgba(17,_17,_26,_0.6)_0px_0px_16px]   relative z-10 h-[5vh] mx-auto ">
           <input
             type="text"
-            className="w-full h-full text-sm text-zinc-300 p-3  placeholder:text-secondary-100 placeholder:text-sm  outline-none bg-transparent"
+            className="w-full h-full text-sm text-zinc-300 p-3 rounded-l-3xl border-yellow-400 border  placeholder:text-secondary-100 placeholder:text-sm  outline-none bg-transparent"
             placeholder="Search"
           />
-          <div className="w-[4rem] flex cursor-pointer rounded-r-3xl bg-yellow-400 justify-center items-center h-full 0">
+          <div className="w-[4rem] flex cursor-pointer rounded-r-3xl   border-yellow-400 border bg-yellow-400 justify-center items-center h-full 0">
             <Icon
               icon="quill:search"
-              className="w-[1.5rem] h-[1.5rem]  object-cover text-zinc-800 pointer-events-none object-center "
+              className="w-[1.4rem] h-[1.4rem]  object-cover text-zinc-800 pointer-events-none object-center "
             />
           </div>
         </div>
       </div>
-      <div className="absolute bottom-10 flex justify-center gap-10 left-[50%] translate-x-[-50%] z-10">
+      <div className="absolute bottom-10 flex justify-center gap-10 left-[50%] translate-x-[-50%] z-20">
         <div className="flex  gap-3 justify-center items-center">
           <span className="flex gap-2 text-secondary-50 text-sm items-center">
             Associated with <span className="text-lg text-yellow-400">|</span>{" "}
